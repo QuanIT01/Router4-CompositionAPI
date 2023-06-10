bcf92c088cf8e85b42c383a6c1bc5e39103d4727
QuanIT01 <n.h.quan126@gmail.com>
2023-06-10 23:22:19 +0700

    Computed in Composition API

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,47 +1,63 @@
 <template>
   <h1>Overview is here</h1>
-  <p>{{ secondName }}</p>
+  <!-- <p>{{ secondName }}</p> -->
   <!-- <p>{{ firstName }}</p> -->
-  <p>{{ car }}</p>
-  <button @click="onChangeSomething">Click me</button>
+  <!-- <p>{{ car }}</p> -->
+  <input type="text" v-model="searchText" />
+  <ul>
+    <li v-for="(customer, index) in customersFiled" :key="index">
+      {{ customer }}
+    </li>
+  </ul>
+  <!-- <button @click="onChangeSomething">Click me</button> -->
 </template>
 <script>
-import { ref, reactive } from "vue";
+import { computed, ref, reactive } from "vue";
+
 export default {
   setup() {
-    const firstName = ref("skybert");
-    const secondName = ref({
-      name: "misa",
-      something: "product",
-    });
-    let car = reactive({
-      price: 10000,
-      name: "Mec",
-    });
-    console.log(secondName); // thay doi
-    console.log(car); // ko thay doi
-    function onChangeSomething() {
-      secondName.value = {
-        name: "Nguyen Quan",
-        something: "ok",
-      };
-      car = {
-        price: 300000,
-        name: "BMW",
-      };
-      // console.log("running here ...");
-      // car.price = 20000;
-      // secondName.value.name = "aaa";
-      // firstName.value = "Nguyen Quan";
-      // console.log(secondName);
-      //  console.log(firstName);
-      // console.log(car);
-    }
+    const searchText = ref("");
+    // const secondName = ref({
+    //   name: "misa",
+    //   something: "product",
+    // });
+    // let car = reactive({
+    //   price: 10000,
+    //   name: "Mec",
+    // });
+    const customers = reactive(["Something", "Nguyen quan", "Haui"]);
+    const customersFiled = computed(() =>
+      customers
+        .map((customer) => {
+          customer = customer.toLowerCase();
+          return customer;
+        })
+        .filter((customer) => customer.includes(searchText.value.toLowerCase()))
+    );
+    // console.log(secondName); // thay doi
+    // console.log(car); // ko thay doi
+    // function onChangeSomething() {
+    //   secondName.value = {
+    //     name: "Nguyen Quan",
+    //     something: "ok",
+    //   };
+    //   car = {
+    //     price: 300000,
+    //     name: "BMW",
+    //   };
+    //   // console.log("running here ...");
+    //   // car.price = 20000;
+    //   // secondName.value.name = "aaa";
+    //   // firstName.value = "Nguyen Quan";
+    //   // console.log(secondName);
+    //   //  console.log(firstName);
+    //   // console.log(car);
+    // }
     return {
-      car,
-      firstName,
-      secondName,
-      onChangeSomething,
+      customersFiled,
+      searchText,
+      // secondName,
+      // onChangeSomething,
     };
   },
 };
