4875c5324603bd7d0d953dc0561cb6b692f1b092
QuanIT01 <n.h.quan126@gmail.com>
2023-06-10 22:26:42 +0700

    Reactivity data with Ref()

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>Overview is here</h1>
+  <p>{{ firstName }}</p>
+  <button @click="onChangeSomething">Click me</button>
 </template>
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const firstName = ref("skybert");
+    function onChangeSomething() {
+      console.log("running here ...");
+      firstName.value = "Nguyen Quan";
+    }
+    return {
+      firstName,
+      onChangeSomething,
+    };
+  },
+};
+</script>
