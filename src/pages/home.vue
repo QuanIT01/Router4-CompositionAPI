0be99b5f5c52a9e247ab1ef6a8dba35c65d1c2ec
QuanIT01 <n.h.quan126@gmail.com>
2023-06-10 23:01:29 +0700

    Reactive() and difference with Ref()

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,19 +1,46 @@
 <template>
   <h1>Overview is here</h1>
-  <p>{{ firstName }}</p>
+  <p>{{ secondName }}</p>
+  <!-- <p>{{ firstName }}</p> -->
+  <p>{{ car }}</p>
   <button @click="onChangeSomething">Click me</button>
 </template>
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 export default {
   setup() {
     const firstName = ref("skybert");
+    const secondName = ref({
+      name: "misa",
+      something: "product",
+    });
+    let car = reactive({
+      price: 10000,
+      name: "Mec",
+    });
+    console.log(secondName); // thay doi
+    console.log(car); // ko thay doi
     function onChangeSomething() {
-      console.log("running here ...");
-      firstName.value = "Nguyen Quan";
+      secondName.value = {
+        name: "Nguyen Quan",
+        something: "ok",
+      };
+      car = {
+        price: 300000,
+        name: "BMW",
+      };
+      // console.log("running here ...");
+      // car.price = 20000;
+      // secondName.value.name = "aaa";
+      // firstName.value = "Nguyen Quan";
+      // console.log(secondName);
+      //  console.log(firstName);
+      // console.log(car);
     }
     return {
+      car,
       firstName,
+      secondName,
       onChangeSomething,
     };
   },
